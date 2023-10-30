75a00e0c40d2f62e22407d51db9d949133e3f6c8
marshall-bits <kz_@hotmail.es>
2023-10-30 14:45:34 +0100

    exercise done

--- exercise-fetch.js
+++ exercise-fetch.js
@@ -12,7 +12,10 @@ console.log("JS is working!");
 
 fetch("https://rickandmortyapi.com/api/character")
     .then((response) => response.json())
-    .then((jsonData) => console.log(jsonData.results))
+    .then((jsonData) => {
+        console.log(jsonData.results)
+        
+    })
     // note for Marcel: Don't forget the catch
     .catch((error) => { console.log(error); })
 
@@ -22,10 +25,24 @@ async function getCharacters() {
     try {
         const data = await fetch("https://rickandmortyapi.com/api/character");
         const jsonData = await data.json();
-        console.log(jsonData.results)
+        console.log(jsonData.results);
+        printData(jsonData);
     } catch (error) {
         console.error(error);
     }
 }
 // REMEMBER TO CALL THE FUNCTION!
 getCharacters()
+
+function printData(jsonData){
+    const ulElement = document.querySelector("#list");
+    jsonData.results.forEach(element => {
+        const liElement = document.createElement("li");
+        liElement.innerHTML = `
+            <h3>${element.name}</h3>
+            <img src=${element.image} alt=${element.name} >
+        `
+        ulElement.appendChild(liElement);
+        console.log(element);
+    });
+}

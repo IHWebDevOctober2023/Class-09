62aea2323d2e6b17ce96a7a98177ed9136669f21
marshall-bits <kz_@hotmail.es>
2023-10-30 12:47:02 +0100

    add first part of the exercise

--- exercise-fetch.js
+++ exercise-fetch.js
@@ -7,3 +7,25 @@
 // Create a ul and display the name of the character and the image inside an li
 
 // BONUS: add some styles
+
+console.log("JS is working!");
+
+fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((jsonData) => console.log(jsonData.results))
+    // note for Marcel: Don't forget the catch
+    .catch((error) => { console.log(error); })
+
+
+// SAME METHOD USING ASYNC AWAIT 👇
+async function getCharacters() {
+    try {
+        const data = await fetch("https://rickandmortyapi.com/api/character");
+        const jsonData = await data.json();
+        console.log(jsonData.results)
+    } catch (error) {
+        console.error(error);
+    }
+}
+// REMEMBER TO CALL THE FUNCTION!
+getCharacters()
